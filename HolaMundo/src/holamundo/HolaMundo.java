214feac18060fbfaa5d5f160b27c793a10278f33
yojhanyac <yac@live.com.ar>
2021-06-05 13:31:00 -0300

    update

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,20 @@
+package holamundo;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola Mundo! :)");
+        System.out.println("Bienvenido al curso de Java");
+        System.out.println("Utilizando NetBeans");
+
+        // int
+        // double
+        // Boolean
+        // char
+        // String
+        // long
+
+    }
+    
+}
